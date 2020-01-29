41f06cc73963d933cb3fabad8f928c31d8cef16c
Vasili (Varzhak) Balyka <varzhakmail@gmail.com>
2020-01-29 10:46:22 +0300

    Remake Request to Get and Post

--- src/main/kotlin/vasili/balyka/springboottest/LibraryController.kt
+++ src/main/kotlin/vasili/balyka/springboottest/LibraryController.kt
@@ -1,34 +1,35 @@
 package vasili.balyka.springboottest
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
 
 @RestController
 @RequestMapping("/library")
 class LibraryController(val bookRepository: Library) {
 
-    @RequestMapping("/allbooks")
-    fun getAll(): ArrayList<*> = bookRepository.library()
+    @GetMapping("/allbooks")
+    fun getAll(): MutableIterable<*> = bookRepository.library()
 
-    @RequestMapping("/freebooks")
+    @GetMapping("/freebooks")
     fun getFree(): MutableIterable<Book> = bookRepository.freeBooks()
 
-    @RequestMapping("/reserve")
-    fun reserve(@RequestParam bookId: String, @RequestParam userId: String): Any =
+    @PostMapping("/reserve")
+    fun reserve(@RequestParam bookId: String, @RequestParam userId: String): Boolean =
         try {
             bookRepository.reserveBook(bookId.toLong() , userId.toLong())
         } catch (e: NumberFormatException) {
-            "bookId and userId must be integer numbers"
+            throw MNFE("bookId and userID must be integer numbers")
         }
 
-
-    @RequestMapping("/unreserve")
-    fun unReserve(@RequestParam bookId: String): Any =
+    @PostMapping("/unreserve")
+    fun unReserve(@RequestParam bookId: String): Boolean =
         try {
             bookRepository.unReserveBook(bookId.toLong())
         } catch (e: NumberFormatException) {
-            "bookId must be an integer number"
+            throw MNFE("bookId must be an integer number")
         }
 }
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+class MNFE(s: String): java.lang.NumberFormatException(s)
